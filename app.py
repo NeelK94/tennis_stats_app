7d1e5c8b2bc4774b6d0a88a1a7a32e010797d161
Neelk94 <neel.khokhar94@gmail.com>
2023-04-12 17:39:29 +0100

    .

--- app.py
+++ app.py
@@ -39,29 +39,47 @@ class Match(db.Model):
     __tablename__ = "Matches"
 
     match_id = db.Column(db.Integer, primary_key=True)
-    set_num = db.Column(db.Integer)
-    game_num = db.Column(db.Integer)
+    player_1_id = db.Column(db.Integer)
+    player_2_id = db.Column(db.Integer)
+    time_stamp = db.Column(db.DateTime)
+    winner = db.Column(db.Integer)
+    loser = db.Column(db.Integer)
+    status = db.Column(db.String)  # Unsent, Sent, Verified, Declined
+
+    def __init__(self, player_1_id, player_2_id, time_stamp, winner, loser):
+        self.player_1_id = player_1_id
+        self.player_2_id = player_2_id
+        self.time_stamp = time_stamp
+        self.winner = winner
+        self.loser = loser
+        self.status = "Unsent"
+
+
+
+# Points Class/Model
+
+class Points(db.Model):
+    __tablename__ = "Points"
+
+    point_id = db.Column(db.Integer, primary_key=True)
+    match_id = db.Column(db.Integer)
+
+    point_num = db.Column(db.Integer)
+
+    server_id = db.Column(db.Integer)
+    receiver_id = db.Column(db.Integer)
+
+    server_sets = db.Column(db.Integer)
+    receiver_sets = db.Column(db.Integer)
+    server_games = db.Column(db.Integer)
+    receiver_games = db.Column(db.Integer)
     server_score = db.Column(db.String)
     receiver_score = db.Column(db.String)
-    server = db.Column(db.Integer)  # ref user with id
-    receiver = db.Column(db.Integer)  # ref user with id
+
     first_serve_outcome = db.Column(db.String)
     second_serve_outcome = db.Column(db.String)
-    winner = db.Column(db.Integer)  # ref user with id
 
-    def __init__(self, player_one, player_two, starter):
-        self.player_one = player_one
-        self.player_two = player_two
-        if starter == 2:
-            self.server = player_one
-            self.receiver = player_two
-        else:
-            self.server = player_two
-            self.receiver = player_one
-        self.set_num = 1
-        self.game_num = 1
-        self.server_score = "0"
-        self.receiver_score = "0"
+    winner_id = db.Column(db.Integer)
 
 
 # User Schema
@@ -73,28 +91,37 @@ class UserSchema(ma.Schema):
 # Match Schema
 class MatchSchema(ma.Schema):
     class Meta:
-        fields = ('match_id', 'set_num', 'game_num', 'server_score', 'receiver_score', 'server', 'receiver',
-                  'first_serve_outcome', 'second_serve_outcome', 'winner')
+        fields = ('match_id', 'player_1_id', 'player_2_id', 'time_stamp', 'winner', 'loser', 'status')
 
 
-# verified / unverified
+# Points Schema
+class PointsSchema(ma.Schema):
+    class Meta:
+        fields = ('point_id', 'match_id', 'point_num', 'server_id', 'receiver_id', 'server_sets', 'receiver_sets',
+                  'server_games', 'receiver_games', 'server_score', 'receiver_score', 'first_serve_outcome',
+                  'second_serve_outcome', 'winner_id')
+
+
+# Use query parameters
 # login API
 # header for every request will include header "basic auth". Verify before you run code.
-
 
 
 # Init Single Schemas
 user_schema = UserSchema()
 match_schema = MatchSchema()
+point_schema = PointsSchema()
+
 
 # Init Multiple Schemas
 users_schema = UserSchema(many=True)
 matches_schema = MatchSchema(many=True)
+points_schema = PointsSchema(many=True)
 
 
 # When first starting the db:
 with app.app_context():
-    db.drop_all()
+    #db.drop_all()
     db.create_all()
 
 
@@ -103,102 +130,28 @@ def homepage():
     return "Home"
 
 
-# Add new match
-@app.route('/match', methods=['POST'])
-def add_match():
-    player_one = request.json['player1']  # id
-    player_two = request.json['player2']  # id
-    starter = request.json['starter']
-
-    new_match = Match(player_one, player_two, starter)
-
-    db.session.add(new_match)
-    db.session.commit()
-
-    return match_schema.jsonify(new_match)
-
-
-# Update match (add in a point)
-@app.route('/match/<int:id>', methods=['PUT'])
-def add_point(id):
-    match = Match.query.get(id)
-
-    set_num = request.json['set']
-    game_num = request.json['game']
-    server_score = request.json['server_score']
-    receiver_score = request.json['receiver_score']
-    server = request.json['server']
-    receiver = request.json['receiver']
-    first_serve_outcome = request.json['first_serve_outcome']
-    second_serve_outcome = request.json['second_serve_outcome']
-    winner = request.json['winner']
-
-    match.set_num = set_num
-    match.game_num = game_num
-    match.server_score = server_score
-    match.receiver_score = receiver_score
-    match.server = server
-    match.receiver = receiver
-    match.first_serve_outcome = first_serve_outcome
-    match.second_serve_outcome = second_serve_outcome
-    match.winner = winner
-
-    db.session.commit()  # No need to add before you commit, since its a PUT
-
-    return winner
-'''
-
-# Update match 2 (add in a point and calculate details)
-@app.route('/match/<int:id>', methods=['PUT'])
-def add_point_2(id):
-    match = Match.query.get(id)
-
-    first_serve_outcome = request.json['first_serve_outcome']
-    second_serve_outcome = request.json['second_serve_outcome']
-    winner = request.json['winner']
-
-    match.first_serve_outcome = first_serve_outcome
-    match.second_serve_outcome = second_serve_outcome
-    match.winner = winner
-
-    # Evaluate match context
-    match.set_num = set_num
-    match.game_num = game_num
-    match.server_score = server_score
-    match.receiver_score = receiver_score
-    match.server = server
-    match.receiver = receiver
-
-    db.session.commit()  # No need to add before you commit, since its a PUT
-
-    return winner
-'''
-
-
-@app.route('/match', methods=['GET'])
-def list_matches():
-    matches = Match.query.all()
-    result = matches_schema.dump(matches)
+# Get all users
+@app.route('/user', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
     return jsonify(result)
 
 
-@app.route('/match/<int:id>', methods=['GET'])
-def get_match(id):
-    match = Match.query.get(id)
-    result = user_schema.jsonify(match)
+# Get single users
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    result = user_schema.jsonify(user)
     return result
 
 
-@app.route('/match/<int:id1>/<int:id2>', methods=['GET'])
-def get_matches(id1, id2):
-    matches = Match.query.filter(
-        or_(
-            and_(Match.server == id1, Match.receiver == id2),
-            and_(Match.server == id2, Match.receiver == id1)
-        )
-    ).all()
-
-    return matches_schema.jsonify(matches)
+# Get single users by name
+@app.route('/user/<string:name>', methods=['GET'])
+def get_user_by_name(name):
+    user = User.query.filter(User.username == name).all()[0]
+    result = user_schema.jsonify(user)
+    return result
 
 
 @app.route('/user', methods=['POST'])
@@ -213,30 +166,6 @@ def add_user():
     db.session.commit()
 
     return user_schema.jsonify(new_user)
-
-
-# Get all users
-@app.route('/user', methods=['GET'])
-def get_users():
-    all_users = User.query.all()  # This is an sqlquery query! No SQL
-    result = users_schema.dump(all_users)
-    return jsonify(result)
-
-
-# Get single users by name
-@app.route('/user/<string:name>', methods=['GET'])
-def get_user_by_name(name):
-    user = User.query.filter(User.username == name).all()[0]
-    result = user_schema.jsonify(user)
-    return result
-
-
-# Get single users
-@app.route('/user/<int:id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-    result = user_schema.jsonify(user)
-    return result
 
 
 # Update user
@@ -267,6 +196,56 @@ def delete_user(id):
     db.session.commit()
     result = user_schema.jsonify(user)
     return result
+
+
+@app.route('/match', methods=['GET'])
+def list_matches():
+    matches = Match.query.all()
+    result = matches_schema.dump(matches)
+    return jsonify(result)
+
+
+@app.route('/match/<int:id>', methods=['GET'])
+def get_match(id):
+    match_list = Match.query.filter(Match.match_id == id).all()
+    result = matches_schema.dump(match_list)
+    return jsonify(result)
+
+
+@app.route('/match/<int:id')
+
+
+@app.route('/match/<int:id1>/<int:id2>', methods=['GET'])
+def get_matches(id1, id2):
+    matches = Match.query.filter(
+        or_(
+            and_(Match.server == id1, Match.receiver == id2),
+            and_(Match.server == id2, Match.receiver == id1)
+        )
+    ).all()
+
+    return matches_schema.jsonify(matches)
+
+
+# Add match
+@app.route('/match', methods=['POST'])
+def add_match():
+    player_1_id = request.json['player_1_id']
+    player_2_id = request.json['player_2_id']
+    time_stamp = request.json['time_stamp']
+    winner = request.json['winner']
+    loser = request.json['loser']
+
+    new_match = Match(player_1_id, player_2_id, time_stamp, winner, loser)
+    db.session.add(new_match)
+    db.session.commit()
+
+    return match_schema.jsonify(new_match)
+
+
+
+
+
 
 
 if __name__ == '__main__':
